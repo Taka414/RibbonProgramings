01bb7d115ae3358cad1c4f79f238f6f395c763d1
Taka <yyyymmddhhmmsssss@gmail.com>
2017-06-26 19:33:23 +0900

    Init

---
+++ FluentRibbons/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ribbons.Sample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        // コンテキストタブの表示切替
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.tabGroup1.Visibility == Visibility.Visible)
+            {
+                this.tabGroup1.Visibility = Visibility.Collapsed;
+                this.tabGroup2.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                this.tabGroup1.Visibility = Visibility.Visible;
+                this.tabGroup2.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RibbonGroupBox_LauncherClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi", "Dialog");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Fluent.Ribbon でリボンプログラミング
+
+Fluent.Ribbon ライブラリを使用したプログラミング
+
+http://takachan.hatenablog.com/entry/2017/06/26/191607
+
